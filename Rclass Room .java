1e0972f017a34ef75dcd1a7a1403000546daa93c
keluthaksash <136364178+keluthaksash@users.noreply.github.com>
2023-06-13 03:31:52 -0700

    Add files via upload

---
+++ Rclass Room .java
@@ -0,0 +1,30 @@
+class Room 
+{
+int length,breadth;
+Room (int x,int y)
+{
+length=x;
+breadth=y;
+}
+int area()
+}
+return(length.*breadth);
+}
+class classRoom extends Room
+{
+int height;
+classRoom(int x,int y,int z){
+super (x,y);
+height=z;
+}
+return (length*breadth*height);
+}
+}
+class subclass}
+public static void main(String[]args)
+}
+classRoom cr=new classRoom(20,30,10);
+System.out.println("Area="+area);
+System.out.println("valume="valume);
+}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
